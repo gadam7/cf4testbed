832ecc4dd95c29ef2ac92af86dd7b9125f91765d
gadam7 <g.adamidis1985@gmail.com>
2023-06-15 22:19:45 +0300

    v0.1

---
+++ chapter-11/example1.js
@@ -0,0 +1,8 @@
+let x = 5;
+let y = 12;
+
+console.log(x, y, x+y);
+document.write("Hello Javascript", x+y);
+document.write("<br>");
+// alert("Hello CF");
+document.write(`Hello Coding Factory ${x+y}`)
---
+++ chapter-11/example6.js
@@ -0,0 +1,16 @@
+let isRaining = 0;
+let lightsAreOn; 
+
+if (isRaining) {
+  lightsAreOn = true;
+} else {
+  lightsAreOn = false;
+}
+
+if (lightsAreOn) {
+  //document.write('Lights are on');
+  console.log('Lights are on');
+} else {
+  //document.write('Lights are not on');
+  console.log('Lights are not on');
+}
